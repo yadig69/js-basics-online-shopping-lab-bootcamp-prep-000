694961bbd60787553d247e583489edb62a94bb03
Gregory Singleton <yadig69design@gmail.com>
2020-07-07 06:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ var itemObj = {itemName:`${item}`, itemPrice: Math.floor(Math.random()*100)}
   return itemObj.itemName + " has been added to your cart."
 }
 
-// function viewCart() {
-//   // write your code here
+function viewCart() {
+  // write your code here
+  
+  function viewCart() {
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  }
+}
   
 //   if (cart.length === 0) {
 //     return "Your shopping cart is empty."
