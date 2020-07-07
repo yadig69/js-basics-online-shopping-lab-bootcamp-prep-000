6d1713a815418c6ad2c92b5b4b3b8ec76c14b9de
Gregory Singleton <yadig69design@gmail.com>
2020-07-07 06:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,10 +35,13 @@ function viewCart() {
   }
 }
   
-
-
 function total() {
   // write your code here
+  total = 0;
+  for(var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
